24010fbbd26f6f36e00eaac4e0c4c3a85aed3981
pratik2820 <pratikmorge28@gmail.com>
2023-10-09 19:48:45 +0530

    try1

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,49 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+
+import React, { useState, useEffect } from "react";
+import './../styles/App.css';
 
 const App = () => {
+    const cityList = [{ name: 'Goa', country: 'India' },
+    { name: 'Amsterdam', country: 'Netherlands' },
+    { name: 'New York', country: 'USA' },
+    { name: 'Darjeeling', country: 'India' },
+    { name: 'Tokyo', country: 'Japan' },
+    { name: 'Lonavala', country: 'India' },
+    { name: 'Brandenburg Gate', country: 'Germany' },
+    { name: 'Reichstag Building', country: 'Germany' },
+    { name: 'Museum Island', country: 'Germany' },
+    { name: 'Munnar', country: 'India' },
+    { name: 'Leh Ladakh', country: 'India' },
+    { name: 'Goa', country: 'India' },
+    { name: 'Agra', country: 'India' },
+    { name: 'Dalhousie', country: 'India' },
+    { name: 'Coorg', country: 'India' },
+    { name: 'Rome', country: 'Italy' },
+    { name: 'Milan', country: 'Italy' },
+    { name: 'Venice', country: 'Italy' },
+    { name: 'Varanasai', country: 'India' },
+    { name: 'Jaipur', country: 'India' },
+    { name: 'The Hofburg', country: 'Austria' },
+    { name: 'Belvedere Palace', country: 'Austria' },
+    { name: 'St. Stephen Cathedral', country: 'Austria' },
+    { name: 'Kahna National Park', country: 'India' },
+    { name: 'Amritsar', country: 'India' },
+    { name: 'Mussoorie', country: 'India' },
+    { name: 'Mount Abu', country: 'India' },
+    { name: 'Tirupati', country: 'India' },
+    ]
+    
   return (
     <div id="main">
-      <h2 data-ns-test="project-name">Recipe Search</h2>
-      <p data-ns-test="project-description">A web application that allows users to search for recipes based on keywords or ingredients. Users can also save their favorite recipes and create a shopping list based on the ingredients needed.</p>
+               {/* Do not remove the main div */}
+        <ol id="location">
+            {
+                cityList.filter((city)=>city.country == "India")
+                .map((city,index)=><li id={'location' + (index+1)}>{city.name}</li>)
+            }
+        </ol>
     </div>
   )
 }
 
-
-export default App;
+export default App
